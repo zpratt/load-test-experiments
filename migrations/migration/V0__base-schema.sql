fb78e6099f0810b4a7b02b6b4d438c36b4828c18
zach pratt <zach@attackofzach.com>
2019-11-23 21:18:33 -0600

    Remove garbage statement for creating the database.
    This should be handled in the init.d scripts

--- migrations/migration/V0__base-schema.sql
+++ migrations/migration/V0__base-schema.sql
@@ -1,12 +1,3 @@
--- CREATE DATABASE test
---     WITH 
---     OWNER = flyway
---     ENCODING = 'UTF8'
---     LC_COLLATE = 'en_US.utf8'
---     LC_CTYPE = 'en_US.utf8'
---     TABLESPACE = pg_default
---     CONNECTION LIMIT = -1;
-
 CREATE TABLE test.pizza
 (
     id uuid,
